a66ed8e5f5a9824d146e70cf9a2627a3b354fd91
Jared Prather <jprather7966@yahoo.com>
2019-07-09 10:12:21 -0500

    Created methods to solve tests

--- CalculatorWithTests/Calculator.cs
+++ CalculatorWithTests/Calculator.cs
@@ -5,23 +5,28 @@ namespace CalculatorWithTests
     {
         public decimal AddNumbersTogether(decimal number1, decimal number2)
         {
-            return 0;
+            return number1 + number2;
         }
         public decimal SubtractNumbers(decimal number1, decimal number2)
         {
-            return 0;
+            return number1 - number2;
         }
         public decimal MulitplyNumbers(decimal num1, decimal num2)
         {
-            return 0;
+            return num1 * num2;
         }
         public decimal DivideNumbers(decimal num1,decimal num2)
         {
-            return 0;
+            return num1 / num2;
         }
         public int Factorial(int number)
         {
-            return 0;
+            int result = number;
+            for (int i = 1; i < number; i++)
+            {
+                result = result * i;
+            }
+            return result;
         }
     }
 }
